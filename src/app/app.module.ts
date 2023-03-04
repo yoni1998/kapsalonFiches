1a592bfb59b486263b2c1f1014e1aa9dabdb182e
YoniVi <yoni.vindelinckx@inetum-realdolmen.world>
2023-03-04 13:37:06 +0100

    init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA--dwTbdrOP41Ikfadz8XdAFRjwSie6dM',
+    authDomain: 'kapsalonmartine-a8cef.firebaseapp.com',
+    projectId: 'kapsalonmartine-a8cef',
+    storageBucket: 'kapsalonmartine-a8cef.appspot.com',
+    messagingSenderId: '338462612120',
+    appId: '1:338462612120:web:b5400c8ba669574117ceed',
+    measurementId: 'G-5Z080T4FP8',
+  },
+};
