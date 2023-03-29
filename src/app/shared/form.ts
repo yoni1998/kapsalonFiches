6195744f05c4b01e6db695a413bb6c6e419acd9d
YoniVi <yoni.vindelinckx@inetum-realdolmen.world>
2023-03-29 10:49:58 +0200

    refactor

--- src/app/shared/form.ts
+++ src/app/shared/form.ts
@@ -50,7 +50,7 @@ export class Form extends UnsubscribeBase {
   phoneNumberValidation(): void {
     if (
       this.formGroupFiches.controls['telefoonNummer'].value?.toString().length >
-        10 ||
+        11 ||
       this.formGroupFiches.controls['telefoonNummer'].value?.toString().length <
         9
     ) {
@@ -63,7 +63,7 @@ export class Form extends UnsubscribeBase {
   mobileNumberValidation(): void {
     if (
       this.formGroupFiches.controls['mobielNummer'].value?.toString().length >
-        10 ||
+        11 ||
       this.formGroupFiches.controls['mobielNummer'].value?.toString().length < 9
     ) {
       this.formGroupFiches.controls['mobielNummer'].setErrors({
@@ -75,7 +75,7 @@ export class Form extends UnsubscribeBase {
   zakelijkNumberValidation(): void {
     if (
       this.formGroupFiches.controls['zakelijkNummer'].value?.toString().length >
-        10 ||
+        11 ||
       this.formGroupFiches.controls['zakelijkNummer'].value?.toString().length <
         9
     ) {
